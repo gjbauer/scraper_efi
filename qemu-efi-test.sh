a3294e132ee43008a3323b59472e2c62294d0d65
Gabriel Bauer <gabeb1277@gmail.com>
2025-11-25 20:55:15 -0500

    RISCV QEMU test...

--- qemu-efi-test.sh
+++ qemu-efi-test.sh
@@ -11,11 +11,24 @@ fi
 sudo mount "$LOOP_DEVICE"p1 efi_image
 sudo mkdir -p efi_image/EFI/BOOT
 # Copy your EFI application to the correct location
-sudo cp target/x86_64-unknown-uefi/release/scraper_efi.efi efi_image/EFI/BOOT/BOOTX64.EFI
+riscv64-linux-gnu-objcopy -O efi-app-riscv64 target/riscv64gc-unknown-none-elf/release/scraper_efi BOOTRISCV64.EFI --subsystem=10
+sudo cp BOOTRISCV64.EFI efi_image/EFI/BOOT/BOOTRISCV64.EFI
 sudo umount efi_image
 
 # Run QEMU with the directory as the FAT filesystem
-qemu-system-x86_64 -m 512M -bios /usr/share/edk2/x64/OVMF.4m.fd -drive format=raw,file=uefi.img
+qemu-system-riscv64 \
+ -M virt,pflash0=pflash0,pflash1=pflash1,acpi=off \
+ -m 4096 -smp 2 \
+ -serial mon:stdio \
+ -device virtio-gpu-pci \
+ -device qemu-xhci \
+ -device usb-kbd \
+ -device virtio-rng-pci \
+ -blockdev node-name=pflash0,driver=file,read-only=on,filename=RISCV_VIRT_CODE.fd \
+ -blockdev node-name=pflash1,driver=file,filename=RISCV_VIRT_VARS.fd \
+ -netdev user,id=net0 \
+ -device virtio-net-pci,netdev=net0 \
+ -device virtio-blk-device,drive=hd0 \
+ -drive file=uefi.img,format=raw,id=hd0,if=none
 
-qemu-system-x86_64 -m 2G --enable-kvm -cpu host -smp 4 -cdrom ~/Downloads/ubuntu*.iso -boot order=d -drive format=raw,file=uefi.img -net nic,model=virtio -net user -display sdl,gl=on
 
